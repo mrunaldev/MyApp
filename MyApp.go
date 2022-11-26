934bb4165f8cb64d2ea441ef7361ebab924c3628
Mrunal Kanta Muduli <119028284+mrunaldev@users.noreply.github.com>
2022-11-26 10:13:54 +0530

    Update MyApp.go

--- MyApp.go
+++ MyApp.go
@@ -1,7 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+// A —> [B, D, C, E, F]
+// B —> [D, C, E, F]
+// C —> []
+// D —> [E, F]
+// E —> [F]
+// F —> []
+var employeeToManagerMappings map[string]string
 
 func main() {
-	fmt.Println("mrunal chnage")
+
+	// construct a mapping from employee to manager
+	employeeToManagerMappings = map[string]string{"A": "A", "B": "A", "C": "B", "D": "B", "E": "D", "F": "E"}
+	findEmployees()
+
+	// // print contents of the resulting dictionary
+	// for key, value := range result
+	//     print(key, "—>", value)
+}
+
+func findAllReportingEmployees(manager string) string {
+	reportees := ""
+
+	if manager == "" {
+		reportees = ""
+	} else {
+		for emp, mgr := range employeeToManagerMappings {
+			if manager == mgr {
+				if emp != mgr {
+					reportees = reportees + findAllReportingEmployees(emp)
+				} else {
+					continue
+				}
+
+			}
+		}
+
+	}
+
+	// for _, val := range reportees {
+	// 	reportees = append(reportees, findAllReportingEmployees(val))
+	// }
+	return reportees
+}
+
+// Find all employees who directly or indirectly reports to a manager
+func findEmployees() {
+	var managers []string
+	for employee, manager := range employeeToManagerMappings {
+
+		managers = append(managers, manager)
+		managers = append(managers, employee)
+	}
+	sort.Strings(managers)
+	for _, val := range unique(managers) {
+		fmt.Println(val, "->", findAllReportingEmployees(val))
+
+	}
+}
+func unique(intSlice []string) []string {
+	keys := make(map[string]bool)
+	list := []string{}
+	for _, entry := range intSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
 }
