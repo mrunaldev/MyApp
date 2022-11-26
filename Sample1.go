5227835fee392665ed448d8824a255be6155b8be
Mrunal Kanta Muduli <mrunalkanta4u@gmail.com>
2022-11-26 13:33:17 +0530

    added new program

--- Sample1.go
+++ Sample1.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
-func Sample1() {
+func sortList(list []int, c chan []int) {
+	sort.Ints(list)
+	c <- list
+}
 
-	fmt.Println("test")
+func Sample1() {
+	// define the size of the number we want to sort
+	// put the number list into a slice
+	list := []int{-9, -11, 12, 13, 9}
+	fmt.Println("Your digits input : ", list)
+
+	// sort in different go routines
+	c := make(chan []int, 4)
+	go sortList(list, c)
+
+	sortedS1 := <-c
+
+	fmt.Println(sortedS1)
 }
